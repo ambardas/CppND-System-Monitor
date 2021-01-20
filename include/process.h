0936809f0fefbfba60455a8fbb1108f0051274e3
Ambar <ambar@calendar42.com>
2021-01-20 18:58:58 +0100

    add constructor for Process class

--- include/process.h
+++ include/process.h
@@ -2,12 +2,14 @@
 #define PROCESS_H
 
 #include <string>
+#include <unordered_map>
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
 class Process {
  public:
+  Process(int pid, std::unordered_map<int, std::string> uid_user_map);
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
