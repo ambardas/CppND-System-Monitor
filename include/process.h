15ae2172b5f52cc4e46f216671cb71b5927d8b2a
Ambar <ambar@calendar42.com>
2021-01-23 12:52:24 +0100

    add process provate var

--- include/process.h
+++ include/process.h
@@ -3,6 +3,10 @@
 
 #include <string>
 #include <unordered_map>
+
+#include "linux_parser.h"
+
+#define hertz sysconf(_SC_CLK_TCK)
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
@@ -17,9 +21,17 @@ class Process {
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  void Cpu_Mem_Utime();
 
   // TODO: Declare any necessary private members
  private:
+  int pid;
+  std::string user;
+  float cpu_util;
+  std::string ram;
+  long int uptime;
+  std::string command;
+  std::unordered_map<int, std::string> uid_user_map;
 };
 
 #endif
