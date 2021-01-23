1d25a737cab6bafde3a09ff44ecd4b2900f6641f
Ambar <ambar@calendar42.com>
2021-01-23 15:32:28 +0100

    convert process gettr calls to const

--- include/process.h
+++ include/process.h
@@ -14,14 +14,13 @@ It contains relevant attributes as shown below
 class Process {
  public:
   Process(int pid, std::unordered_map<int, std::string> uid_user_map);
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
+  int Pid() const;                         // TODO: See src/process.cpp
+  std::string User() const;                // TODO: See src/process.cpp
+  std::string Command() const;             // TODO: See src/process.cpp
+  float CpuUtilization() const;            // TODO: See src/process.cpp
+  std::string Ram() const;                 // TODO: See src/process.cpp
+  long int UpTime() const;                 // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
-  void Cpu_Mem_Utime();
 
   // TODO: Declare any necessary private members
  private:
@@ -32,6 +31,10 @@ class Process {
   long int uptime;
   std::string command;
   std::unordered_map<int, std::string> uid_user_map;
+
+  void GetUser();
+  void GetCommand();
+  void Cpu_Mem_Utime();
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -16,65 +16,33 @@ using std::vector;
 Process::Process(int pid, std::unordered_map<int, string> uid_user_map)
     : pid(pid), uid_user_map(uid_user_map) {
   Cpu_Mem_Utime();
-  User();
-  Command();
+  GetUser();
+  GetCommand();
 };
 
 // TODO: Return this process's ID
-int Process::Pid() { return pid; }
+int Process::Pid() const { return pid; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return cpu_util; }
+float Process::CpuUtilization() const { return cpu_util; }
 
 // TODO: Return the command that generated this process
-string Process::Command() {
-  string user_file = LinuxParser::kProcDirectory + "/" + to_string(pid) +
-                     LinuxParser::kCmdlineFilename;
-  string line;
-  std::ifstream filestream(user_file);
-  if (filestream.is_open()) {
-    while (std::getline(filestream, line)) {
-      std::istringstream linestream(line);
-      command = line;
-      return line;
-    }
-  }
-  return " ";
-};
+string Process::Command() const { return command; }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return ram; }
+string Process::Ram() const { return ram; }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() {
-  string status_file = LinuxParser::kProcDirectory + "/" + to_string(pid) +
-                       LinuxParser::kStatusFilename;
-  string line;
-  string stat_name;
-  int an_id;
-  std::ifstream filestream(status_file);
-  if (filestream.is_open()) {
-    while (std::getline(filestream, line)) {
-      std::istringstream linestream(line);
-      while (linestream >> stat_name >> an_id) {
-        if (stat_name == "Uid:") {
-          user = uid_user_map[an_id];
-          return user;
-        }
-      }
-    }
-  }
-  return " ";
-};
+string Process::User() const { return user; }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return uptime; }
+long int Process::UpTime() const { return uptime; }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a [[maybe_unused]]) const {
-  // return a.cpu_util < this->cpu_util;
-  return true;
+bool Process::operator<(Process const& a) const {
+  return a.CpuUtilization() > this->CpuUtilization();
+  // return true;
 }
 
 void Process::Cpu_Mem_Utime() {
@@ -105,9 +73,40 @@ void Process::Cpu_Mem_Utime() {
 
       uptime = stat14 + stat15 + stat16 + stat17;
       int temp_ram = stat23 / 1000000;
-      ram = to_string(temp_ram) + " MB";
+      ram = to_string(temp_ram);
       double temp_seconds = LinuxParser::UpTime() - (stat22 / hertz);
       cpu_util = (uptime / hertz) / temp_seconds;
+    }
+  }
+}
+
+void Process::GetUser() {
+  string status_file = LinuxParser::kProcDirectory + "/" + to_string(pid) +
+                       LinuxParser::kStatusFilename;
+  string line;
+  string stat_name;
+  int an_id;
+  std::ifstream filestream(status_file);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> stat_name >> an_id) {
+        if (stat_name == "Uid:") {
+          user = uid_user_map[an_id];
+        }
+      }
+    }
+  }
+}
+
+void Process::GetCommand() {
+  string user_file = LinuxParser::kProcDirectory + "/" + to_string(pid) +
+                     LinuxParser::kCmdlineFilename;
+  string line;
+  std::ifstream filestream(user_file);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      command = line;
     }
   }
 }
