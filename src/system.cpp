0dbc4756f668346bd9a8ea8d0ba512bc664eb549
Ambar <ambar@calendar42.com>
2021-01-23 12:54:12 +0100

    return system process counts and uptime

--- src/system.cpp
+++ src/system.cpp
@@ -4,10 +4,7 @@
 #include <string>
 #include <vector>
 
-#include "process.h"
-#include "processor.h"
 #include "system.h"
-#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
@@ -38,10 +35,10 @@ float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
+int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+long System::UpTime() { return LinuxParser::UpTime(); }
