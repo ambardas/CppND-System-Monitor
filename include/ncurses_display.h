ec38ea004a9c20a9a91272591f1092494e89feac
ambardas <ambardas@users.noreply.github.com>
2021-01-29 18:00:02 +0100

    update number of lines of ncurses display

--- include/ncurses_display.h
+++ include/ncurses_display.h
@@ -7,7 +7,9 @@
 #include "system.h"
 
 namespace NCursesDisplay {
-void Display(System& system, int n = 40);
+void Display(System& system, int n = 10); 
+// If you want to change the number of processes to be displayed in the main window 
+// change the above n = 10 to a different number.  
 void DisplaySystem(System& system, WINDOW* window);
 void DisplayProcesses(std::vector<Process>& processes, WINDOW* window, int n);
 std::string ProgressBar(float percent);
